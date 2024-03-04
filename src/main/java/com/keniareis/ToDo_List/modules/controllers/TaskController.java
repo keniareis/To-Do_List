96451598baddcdd6663310e2cd2786005434bf64
keniareis <keniapubg@gmail.com>
2024-03-04 13:08:24 -0300

    adicionando os arquivos do front

--- src/main/java/com/keniareis/ToDo_List/modules/controllers/TaskController.java
+++ src/main/java/com/keniareis/ToDo_List/modules/controllers/TaskController.java
@@ -1,4 +1,9 @@
 package com.keniareis.ToDo_List.modules.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class TaskController {
 }
